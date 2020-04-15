ec1f0b53bf7458ef47200927b8d6638ad7da78f4
ahmedalwardani <aalwa049@uottawa.ca>
2020-04-15 16:26:37 -0400

    use selectors to transform data

---
+++ src/helpers/selectors.js
@@ -0,0 +1,23 @@
+export function getAppointmentsForDay(state, day) {
+  if (state.days.length === 0) {
+    return [];
+  }
+
+  let isFound = false;
+  for (let i = 0; i < state.days.length; i++) {
+    if (state.days[i].name === day) {
+      isFound = true;
+      break;
+    }
+  }
+  if (!isFound) return [];
+
+  const dayAppointments = state.days.filter(weekDay => weekDay.name === day)[0]
+    .appointments;
+
+  const dayAppointmentsArray = [];
+  for (const dayAppointment of dayAppointments) {
+    dayAppointmentsArray.push(state.appointments[dayAppointment]);
+  }
+  return dayAppointmentsArray;
+}
