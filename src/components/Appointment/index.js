17f83fc4a2d7ec9f7e1fbb6c1299edc23945302c
ahmedalwardani <aalwa049@uottawa.ca>
2020-04-17 15:46:57 -0400

    add interviewers selector

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,14 +2,32 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
+import useVisualMode from "../../hooks/useVisualMode";
 
 import "./styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show {...props.interview} /> : <Empty />}
+      {mode === CREATE && (
+        <Form interviewers={props.interviewers} onCancel={() => back()} />
+      )}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   );
 }
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export default function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
   if (state.days.length === 0) {
     return [];
   }
@@ -20,4 +20,38 @@ export default function getAppointmentsForDay(state, day) {
     dayAppointmentsArray.push(state.appointments[dayAppointment]);
   }
   return dayAppointmentsArray;
+}
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  return {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  };
+}
+
+export function getInterviewersForDay(state, day) {
+  if (state.days.length === 0) {
+    return [];
+  }
+
+  let isFound = false;
+  for (let i = 0; i < state.days.length; i++) {
+    if (state.days[i].name === day) {
+      isFound = true;
+      break;
+    }
+  }
+  if (!isFound) return [];
+
+  const dayInterviewers = state.days.filter(weekDay => weekDay.name === day)[0]
+    .interviewers;
+
+  const dayInterviewersArray = [];
+  for (const dayInterviewer of dayInterviewers) {
+    dayInterviewersArray.push(state.interviewers[dayInterviewer]);
+  }
+  return dayInterviewersArray;
 }
