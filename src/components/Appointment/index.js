0b933da893e1b54496e10f02600eb2629e69bcf2
ahmedalwardani <aalwa049@uottawa.ca>
2020-04-17 23:31:31 -0400

    add handling errors

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import Form from "components/Appointment/Form";
+import Error from "components/Appointment/Error";
 import Saving from "components/Appointment/Saving";
 import Deleting from "components/Appointment/Deleting";
 import useVisualMode from "../../hooks/useVisualMode";
@@ -17,6 +18,8 @@ const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
 const EDITING = "EDITING";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -32,12 +35,15 @@ export default function Appointment(props) {
     props
       .bookInterview(props.id, interview)
       .then(() => transition(SHOW))
-      .catch(err => console.log(err));
+      .catch(() => transition(ERROR_SAVE, true));
   }
 
   function deleteInterview() {
-    transition(DELETING);
-    props.cancelInterview(props.id).then(() => transition(EMPTY));
+    transition(DELETING, true);
+    props
+      .cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch(() => transition(ERROR_DELETE, true));
   }
 
   return (
@@ -68,8 +74,13 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer.id}
         />
       )}
+
       {mode === SAVING && <Saving />}
       {mode === DELETING && <Deleting />}
+      {mode === ERROR_DELETE && (
+        <Error message="Could not cancel appointment" onClose={() => back()} />
+      )}
+      {mode === ERROR_SAVE && <Error message="Could not update appointment" />}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
